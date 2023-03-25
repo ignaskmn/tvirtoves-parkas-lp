7c927421d260a1f687033bb1d3a2c3c63bad2528
ignaskmn <ignas@kmn.lt>
2023-03-26 00:12:01 +0200

    no session triggers forward to /login

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,6 +6,7 @@ import {
 } from "$env/static/public";
 import { createSupabaseServerClient } from "@supabase/auth-helpers-sveltekit";
 import type { Handle } from "@sveltejs/kit";
+import { redirect, error } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.supabase = createSupabaseServerClient({
@@ -25,6 +26,13 @@ export const handle: Handle = async ({ event, resolve }) => {
     } = await event.locals.supabase.auth.getSession();
     return session;
   };
+
+  if (event.url.pathname.startsWith("/admin")) {
+    const session = await event.locals.getSession();
+    if (!session) {
+      throw redirect(303, "/login");
+    }
+  }
 
   return resolve(event, {
     /**
